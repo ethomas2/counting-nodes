50b99324de0852f419080b2f793ed6d7897f9b85
Evan Thomas <evanrthom@gmail.com>
2018-09-27 16:46:50 -0700

    Added main.2.go

---
+++ main.2.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type TNode struct {
+	gNode   uint64
+	bitmask uint64
+}
+
+type tuple struct {
+	fst int
+	snd int
+}
+
+func dfsTree(root TNode, getChildren func(TNode) []TNode) (int, int) {
+	children := getChildren(root)
+	if len(children) == 0 {
+		return 1, 1
+	}
+
+	nNodes, nLeaves := 0, 0
+	for _, child := range children {
+		x, y := dfsTree(child, getChildren)
+		nNodes += x
+		nLeaves += y
+	}
+	return nNodes + 1, nLeaves
+}
+
+func solve(gNode, nRows, nCols uint64) (int, int) {
+	getChildren := func(tnode TNode) []TNode {
+		deltas := []tuple{
+			tuple{1, 0},
+			tuple{-1, 0},
+			tuple{0, 1},
+			tuple{0, -1},
+		}
+		var children []TNode
+		for _, delta := range deltas {
+			var deltar, deltac int = delta.fst, delta.snd
+			var r, c uint64 = tnode.gNode / nCols, tnode.gNode % nCols
+			ooBounds := ((r <= 0 && deltar == -1) ||
+				(c <= 0 && deltac == -1) ||
+				(r >= nRows-1 && deltar == 1) ||
+				(c >= nCols-1 && deltac == 1))
+			if ooBounds {
+				continue
+			}
+			r += uint64(deltar)
+			c += uint64(deltac)
+
+			newGNode := r*nCols + c
+			visited := (1<<newGNode)&tnode.bitmask != 0
+			if visited {
+				continue
+			}
+			children = append(children, TNode{gNode: newGNode, bitmask: tnode.bitmask | (1 << newGNode)})
+		}
+		return children
+	}
+	tnode := TNode{gNode, 1 << gNode}
+	return dfsTree(tnode, getChildren)
+}
+
+func main() {
+	nRows, nCols := mustAToi(os.Args[1]), mustAToi(os.Args[2])
+	// TODO: allow custom start nodes
+	// TODO: make gNodes a type (type gNode int)
+	fmt.Println(solve(0, uint64(nRows), uint64(nCols)))
+
+}
+
+func mustAToi(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+	return i
+
+}
