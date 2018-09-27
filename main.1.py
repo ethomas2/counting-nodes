292346f5e79c9170d75f1a8e343939567f88087c
Evan Thomas <evanrthom@gmail.com>
2018-09-27 12:26:39 -0700

    Minor perf improvements to main.1.py
    Did this mostly to be fair in the comparison between main.1.py and
    main.1.go
    * On recursive calls to dfs, mutate the visited set passed in instead
    of construcrting a new immutable visited set
    * dfs accepts a node as it's first argument isntead of a path

--- main.1.py
+++ main.1.py
@@ -4,11 +4,11 @@ import sys
 N, M = int(sys.argv[1]), int(sys.argv[2])
 start = (0, 0)
 
-def get_options( (x, y), visited_nodes):
+def get_children( (x, y), visited_nodes):
     return [(xprime, yprime) for (xprime, yprime) in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
             if 0 <= xprime < N and 0 <= yprime < M and (xprime, yprime) not in visited_nodes]
 
-def dfs(path_so_far=None, visited_nodes=None):
+def dfs(node, visited_nodes):
     """
     This is dfs-ing through the graph G', NOT G. You can think of each
     invocation of this function as visiting a node in G'. G' is a tree, where G
@@ -17,20 +17,19 @@ def dfs(path_so_far=None, visited_nodes=None):
     number of nodes in the tree rooted at n' and y is the number of terminal
     nodes at the tree rooted at n'.
     """
-    if path_so_far is None:   path_so_far = [ start ]
-    if visited_nodes is None: visited_nodes = set( [start] )
 
-    curr_node = path_so_far[-1]
-    opts      = get_options(curr_node, visited_nodes)
+    children = get_children(node, visited_nodes)
 
-    if len(opts) == 0: return 1, 1
+    if len(children) == 0: return 1, 1
 
     total_paths, total_terminal_paths = 0, 0
-    for opt in opts:
-        n_paths, n_terminal_paths = dfs(path_so_far + [opt], visited_nodes | set( [opt] ) )
+    for child in children:
+        visited_nodes.add(child)
+        n_paths, n_terminal_paths = dfs(child, visited_nodes)
+        visited_nodes.remove(child)
         total_paths               += n_paths
         total_terminal_paths      += n_terminal_paths
 
     return total_paths + 1, total_terminal_paths
 
-print( dfs() )
+print( dfs(start, set( [start] )) )
