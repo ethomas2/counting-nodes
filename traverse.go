1c914d7049c25f53d5432e210aa7e771d3896861
Evan Thomas <evanrthom@gmail.com>
2018-09-28 11:29:18 -0700

    Add traverse.go

---
+++ traverse.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type node int
+
+func push(nodechan chan node, stack *[]node, n node) {
+	select {
+	case nodechan <- n:
+	default:
+		*stack = append(*stack, n)
+	}
+}
+
+func pull(nodechan chan node, stack *[]node) node {
+	select {
+	case n := <-nodechan:
+		return n
+	default:
+		if len(*stack) > 0 {
+			n := (*stack)[len(*stack)-1]
+			*stack = (*stack)[:len(*stack)-1]
+			return n
+		}
+		return <-nodechan
+	}
+}
+
+func traverseTree(nodechan chan node, visit func(node), getChildren func(node) []node) {
+	var stack []node
+	for {
+		node := pull(nodechan, &stack)
+		children := getChildren(node)
+		visit(node)
+		for _, child := range children {
+			push(nodechan, &stack, child)
+		}
+	}
+}
+
+func test() {
+	NUMGOROUTINES := 4
+	c := make(chan node, NUMGOROUTINES+1)
+	visit := func(n node) {
+		fmt.Println(n)
+		time.Sleep(time.Second)
+	}
+	getChildren := func(n node) []node {
+		return []node{2 * n, 2*n + 1}
+	}
+	for i := 0; i < NUMGOROUTINES; i++ {
+		go traverseTree(c, visit, getChildren)
+	}
+	c <- 1
+	time.Sleep(5 * time.Second)
+}
